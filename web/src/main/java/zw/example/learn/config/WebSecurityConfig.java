daf83620a58a7493af106aa929cfba860ca2f433
zw <zw2513@163.com>
2017-02-12 15:47:02 +0800

    todo : add spring security support

---
+++ web/src/main/java/zw/example/learn/config/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package zw.example.learn.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by zw on 2017/2/12 15:43.
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+}
